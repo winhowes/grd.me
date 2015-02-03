45077ae87ef5e2218cd72fb20b08218705aa2de2
Winston <winstonhowes@gmail.com>
2015-02-03 02:16:00 -0500

    fixed minor encoding issue

--- Chrome/src/inject/crypt.js
+++ Chrome/src/inject/crypt.js
@@ -82,7 +82,7 @@ function encrypt(shortEncrypt){
 	}
 	ciphertext = ciphertext.slice(0, - 1); 
 	ciphertext += endTag;
-	ciphertext.replace(/\+/g, ")").replace(/\//g, ":");
+	ciphertext = ciphertext.replace(/\+/g, ")").replace(/\//g, ":");
 	if(shortEncrypt){
 		var actualCiphertext = ciphertext.replace(startTag, "").replace(endTag, "");
 		var rand = getRandomString(64);
@@ -234,7 +234,7 @@ function decrypt(elem, callback){
  * ciphertext: the text excluding the crypto tags to decrypt
 */
 function decryptText(ciphertext){
-	ciphertext.replace(/\)/g, "+").replace(/\:/g, "/");
+	ciphertext = ciphertext.replace(/\)/g, "+").replace(/\:/g, "/");
 	ciphertext = ciphertext.split("|");
 	for(var i=0; i<ciphertext.length; i++){
 		var plaintext;
