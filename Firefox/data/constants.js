3d4f6c67deb766959a2f8c02eea9b3e9e6909675
Winston <winstonhowes@gmail.com>
2015-02-11 14:52:24 -0500

    Test of new decrypt text

--- Firefox/data/constants.js
+++ Firefox/data/constants.js
@@ -1,7 +1,7 @@
 var startTag = '~~grdme~~',
 endTag = '~~!grdme~~',
 NONCE_CHAR = "!",
-DECRYPTED_MARK = "[Decrypted Text]",
+DECRYPTED_MARK = "<grdme style='font-weight:bold;'>&#x1f512;</grdme>",
 UNABLE_TO_DECRYPT = "[Unable to decrypt message]",
 UNABLE_startTag = "[start tag]",
 UNABLE_endTag = "[end tag]";
