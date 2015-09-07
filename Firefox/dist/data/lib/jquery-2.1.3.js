92b8fb0f6d35de47aca3101272b838afbfea159c
Winston Howes <winstonhowes@gmail.com>
2015-09-06 19:56:05 -0700

    Update jquery to conform to amo safety standards - (removed uneccesary bits)

--- Firefox/dist/data/lib/jquery-2.1.3.js
+++ Firefox/dist/data/lib/jquery-2.1.3.js
@@ -313,22 +313,11 @@
 		globalEval: function globalEval(code) {
 			var script,
 			    indirect = function indirect() {};
-
+			console.log('JQUERY WINSTON HERE');
 			code = jQuery.trim(code);
 
 			if (code) {
-				// If the code includes a valid, prologue position
-				// strict mode pragma, execute code by injecting a
-				// script tag into the document.
-				if (code.indexOf("use strict") === 1) {
-					script = document.createElement("script");
-					script.text = code;
-					document.head.appendChild(script).parentNode.removeChild(script);
-				} else {
-					// Otherwise, avoid the DOM node creation, insertion
-					// and removal by using an indirect global eval
-					indirect(code);
-				}
+				indirect(code);
 			}
 		},
 
--- Firefox/src/data/lib/jquery-2.1.3.js
+++ Firefox/src/data/lib/jquery-2.1.3.js
@@ -311,22 +311,11 @@ jQuery.extend({
 	globalEval: function( code ) {
 		var script,
 			indirect = function(){};
-
+			console.log('JQUERY WINSTON HERE');
 		code = jQuery.trim( code );
 
 		if ( code ) {
-			// If the code includes a valid, prologue position
-			// strict mode pragma, execute code by injecting a
-			// script tag into the document.
-			if ( code.indexOf("use strict") === 1 ) {
-				script = document.createElement("script");
-				script.text = code;
-				document.head.appendChild( script ).parentNode.removeChild( script );
-			} else {
-			// Otherwise, avoid the DOM node creation, insertion
-			// and removal by using an indirect global eval
-				indirect( code );
-			}
+			indirect( code );
 		}
 	},
 
